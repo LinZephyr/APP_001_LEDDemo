ef42b7cbbe239d2e9466c75855ea072a1de0fae2
LinZepyhr <229382777@qq.com>
2018-10-02 18:20:00 +0800

    v1, created by android studio

---
+++ app/src/main/java/com/example/cfinvo/app_001_leddemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cfinvo.app_001_leddemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+githubwukelanwukel基辅国立大学 acd 
