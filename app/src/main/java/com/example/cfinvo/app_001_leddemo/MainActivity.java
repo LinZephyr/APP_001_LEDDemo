9d17376f44b2be990666c5ed40506b5f13e9e9c2
LinZepyhr <229382777@qq.com>
2018-10-10 18:38:23 +0800

    v4, add jni

--- app/src/main/java/com/example/cfinvo/app_001_leddemo/MainActivity.java
+++ app/src/main/java/com/example/cfinvo/app_001_leddemo/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
+import com.example.cfinvo.hardlibrary.HardControl;
+
 public class MainActivity extends AppCompatActivity {
 
     private boolean ledon = false;
@@ -20,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     class MyButtonListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
+
+            HardControl hardControl = new HardControl();
+
             ledon = !ledon;
             if(ledon){
                 button.setText("ALL OFF");
@@ -27,6 +32,13 @@ public class MainActivity extends AppCompatActivity {
                 checkBoxled2.setChecked(true);
                 checkBoxled3.setChecked(true);
                 checkBoxled4.setChecked(true);
+
+                /* 调用JNI实现的HardControl类来点亮LED */
+                HardControl.ledCtrl(0, 1);
+                HardControl.ledCtrl(1, 1);
+                HardControl.ledCtrl(2, 1);
+                HardControl.ledCtrl(3, 1);
+                //HardControl.ledCtrl(1, 1);
             }
             else{
                 button.setText("ALL ON");
@@ -34,6 +46,13 @@ public class MainActivity extends AppCompatActivity {
                 checkBoxled2.setChecked(false);
                 checkBoxled3.setChecked(false);
                 checkBoxled4.setChecked(false);
+
+                /* 熄灭 */
+                HardControl.ledCtrl(0, 0);
+                HardControl.ledCtrl(1, 0);
+                HardControl.ledCtrl(2, 0);
+                HardControl.ledCtrl(3, 0);
+                //HardControl.ledCtrl(1, 0);
             }
         }
     }
@@ -45,40 +64,48 @@ public class MainActivity extends AppCompatActivity {
         switch(view.getId()) {
             case R.id.LED1:
                 if (checked){
+                    HardControl.ledCtrl(0, 1);
                     // Put some meat on the sandwich
                     Toast.makeText(getApplicationContext(), "LED1 on", Toast.LENGTH_SHORT).show();
                 }
                 else{
+                    HardControl.ledCtrl(0, 0);
                     Toast.makeText(getApplicationContext(), "LED1 off", Toast.LENGTH_SHORT).show();
                     // Remove the meat
                 }
                 break;
             case R.id.LED2:
                 if (checked){
+                    HardControl.ledCtrl(1, 1);
                     // Put some meat on the sandwich
                     Toast.makeText(getApplicationContext(), "LED2 on", Toast.LENGTH_SHORT).show();
                 }
                 else{
+                    HardControl.ledCtrl(1, 0);
                     Toast.makeText(getApplicationContext(), "LED2 off", Toast.LENGTH_SHORT).show();
                     // Remove the meat
                 }
                 break;
             case R.id.LED3:
                 if (checked){
+                    HardControl.ledCtrl(2, 1);
                     // Put some meat on the sandwich
                     Toast.makeText(getApplicationContext(), "LED3 on", Toast.LENGTH_SHORT).show();
                 }
                 else{
+                    HardControl.ledCtrl(2, 0);
                     Toast.makeText(getApplicationContext(), "LED3 off", Toast.LENGTH_SHORT).show();
                     // Remove the meat
                 }
                 break;
             case R.id.LED4:
                 if (checked){
+                    HardControl.ledCtrl(3, 1);
                     // Put some meat on the sandwich
                     Toast.makeText(getApplicationContext(), "LED4 on", Toast.LENGTH_SHORT).show();
                 }
                 else{
+                    HardControl.ledCtrl(3, 0);
                     Toast.makeText(getApplicationContext(), "LED4 off", Toast.LENGTH_SHORT).show();
                     // Remove the meat
                 }
@@ -90,6 +117,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HardControl.ledOpen();
 
         button = findViewById(R.id.button);
         button.setOnClickListener(new MyButtonListener());
--- app/src/main/java/com/example/cfinvo/hardlibrary/HardControl.java
+++ app/src/main/java/com/example/cfinvo/hardlibrary/HardControl.java
@@ -1,4 +1,19 @@
 package com.example.cfinvo.hardlibrary;
 
 public class HardControl {
+    /* 定义LED控制的三个native方法 */
+    /* 加了static可以省略类名直接在主方法调用,不加则必须先实例化后用实例调用 */
+    public static native int ledCtrl(int which, int status);
+    public static native int ledOpen();
+    public static native void ledClose();
+
+    /* 静态块：加了static只会执行一次,不加static则每调用一次都执行 */
+    /* 可以使用Crtl+Alt+T生成捕获异常的代码 */
+    static {
+        try {
+            System.loadLibrary("hardcontrol");/* 加载库,库名为hardcontrol */
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
